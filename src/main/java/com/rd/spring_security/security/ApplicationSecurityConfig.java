264ed549a0ad9e65986c2e1466f783ca618602d8
ridvan.dogan <ridvan.dogan@kolaysoft.com.tr>
2022-05-26 17:32:41 +0300

    basic auth

---
+++ src/main/java/com/rd/spring_security/security/ApplicationSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.rd.spring_security.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created at 26.05.2022.
+ *
+ * @author Ridvan Dogan
+ */
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}

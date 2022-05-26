9e88ba2227cf5250a4a3627e552919610f8c5b9a
ridvan.dogan <ridvan.dogan@kolaysoft.com.tr>
2022-05-26 17:51:51 +0300

    whitelist some url

--- src/main/java/com/rd/spring_security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/rd/spring_security/security/ApplicationSecurityConfig.java
@@ -17,6 +17,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
